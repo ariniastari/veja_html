58a1e3c230feae333df0a2faf8ecaeea794f4b72
ariniastari <astariarini@gmail.com>
2014-12-16 20:15:22 +0700

    home

---
+++ assets/javascripts/veja.js
@@ -0,0 +1,9 @@
+$('.youtube').click(function(){
+    video = '<iframe width="100%" height="100%" allowfullscreen src="'+ $(this).attr('data-video') +'"></iframe>';
+    $(this).replaceWith(video);
+    $('.sprites.produk-1').addClass('remove');
+});
+
+$('.height').css({
+    'height': $('.youtube').height() 
+});
